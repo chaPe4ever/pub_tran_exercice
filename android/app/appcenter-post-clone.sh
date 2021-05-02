78980ad508e76d6843a868669de8b786b8e14521
Petros Chatzipetrou <petros.chatzipetrou@mckesson.eu>
2021-05-02 20:05:42 +0200

    [refactor] Add appcenter flavor

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -15,13 +15,13 @@ export PATH=`pwd`/flutter/bin:$PATH
 # dart $APPCENTER_SOURCE_DIRECTORY/tool/env.dart
 
 # build APK
-flutter build apk --release
-
+#flutter build apk --release
+flutter build apk --release --flavor appCenter -t lib/main.dart
 # if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
 #flutter build appbundle
 
 # copy the APK where AppCenter will find it
-#mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/appCenter/release/app-appCenter-release.apk $_
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/appCenter/release/app-appCenter-release.apk $_
+#mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
 # copy the AAB where AppCenter will find it
 #mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app.aab $_
